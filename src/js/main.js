1e843a4179dd83d4d9ceedf96b66897671bf7090
Laurin Vasquez <113741390+LaulinVasquez@users.noreply.github.com>
2025-09-20 19:42:32 -0600

    html segmentation done, style in the header and functions from mjs module applied

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,29 @@
 import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
+
+console.log(parkData);
+
+// 1. update the link in the disclaimer area to read the name of the park and navigate to that park's offficial website
+const disclaimer = document.querySelector(".disclaimer > a");
+disclaimer.href = parkData.url;
+
+disclaimer.innerHTML = parkData.fullName;
+
+// 2. update the title of the page to read the name of the park
+document.querySelector("head > title").textContent = parkData.fullName;
+
+// 3 Use the first image in the list in the data for the hero image.
+document.querySelector(".hero-banner > img").src = parkData.images[0].url;
+
+// 4. Update the name, designation, and states of the park in the hero.
+function parkInfoTemplate(info) {
+  return `<a href="/" class="hero-banner__title">${info.name}</a>
+    <p class="hero-banner__subtitle">
+        <span>${info.designation}</span>
+        <span>${info.states}</span>
+    </p>`;
+}
+
+const smallBanner = document.querySelector(".hero-banner__content");
+smallBanner.innerHTML = parkInfoTemplate(parkData);
