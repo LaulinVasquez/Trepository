2fb1dcfaeda8cc19dbe188307bba773fbe935120
Laurin Vasquez <113741390+LaulinVasquez@users.noreply.github.com>
2025-09-28 21:34:34 -0600

    update to module and back end main

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,21 @@
-import { getParkData } from "./parkService.mjs";
+import { getParkData, getParkInfoLinks, getMailingAddress, getVoicePhone } from "./parkService.mjs";
 
 const parkData = getParkData();
+const parkInfoLinks = getParkInfoLinks();
 
-console.log(parkData);
-
-// 1. update the link in the disclaimer area to read the name of the park and navigate to that park's offficial website
-const disclaimer = document.querySelector(".disclaimer > a");
-disclaimer.href = parkData.url;
-
-disclaimer.innerHTML = parkData.fullName;
-
-// 2. update the title of the page to read the name of the park
-document.querySelector("head > title").textContent = parkData.fullName;
-
-// 3 Use the first image in the list in the data for the hero image.
-document.querySelector(".hero-banner > img").src = parkData.images[0].url;
+function setHeaderInfo(data) {
+  // insert data into disclaimer section
+  const disclaimer = document.querySelector(".disclaimer > a");
+  disclaimer.href = data.url;
+  disclaimer.innerHTML = data.fullName;
+  // update the title of the site. Notice that we can select things in the head just like in the body with querySelector
+  document.querySelector("head > title").textContent = data.fullName;
+  // set the banner image
+  document.querySelector(".hero-banner > img").src = data.images[0].url;
+  // use the template function above to set the rest of the park specific info in the header
+  document.querySelector(".hero-banner__content").innerHTML =
+    parkInfoTemplate(data);
+}
 
 // 4. Update the name, designation, and states of the park in the hero.
 function parkInfoTemplate(info) {
@@ -27,3 +28,54 @@ function parkInfoTemplate(info) {
 
 const smallBanner = document.querySelector(".hero-banner__content");
 smallBanner.innerHTML = parkInfoTemplate(parkData);
+
+
+function setParkIntro(data) {
+  const intro = document.querySelector(".intro");
+  intro.innerHTML = `<h1 class="intro__title">${data.fullName}</h1>
+    <p class="intro__description">${data.description}</p>`;
+}
+
+
+function mediaCardTemplate(info) {
+  const infoMap = info
+    .map(
+      (item) => `
+      <img
+    src="${item.image}"
+    alt="${item.name}"
+    class="media-card__img"
+  />
+  <h2 class="media-card__caption">${item.name}</h2>
+  <p>${item.description}</p>
+  `
+    ).join("");
+
+  const info1 = document.querySelector(".info");
+  info1.innerHTML = infoMap;
+  return info1;
+}
+
+function footerTemplate(info) {
+  const mailing = getMailingAddress(info.addresses);
+  const phone = getVoicePhone(info.contacts.phoneNumbers);
+  
+  const footer = document.querySelector("#park-footer");
+  footer.innerHTML = `
+  <section class= contact>
+    <h3>Contact Info</h3>
+    <h4>Mailing Address:</h4>
+    <p>${mailing.line1}</p>
+    <p>${mailing.city}, ${mailing.stateCode} ${mailing.postalCode}</p></div>
+    <h4>Phone:</h4>
+    <p>${phone}</p>
+  </section>  
+  `
+}
+
+
+
+setHeaderInfo(parkData);
+setParkIntro(parkData);
+mediaCardTemplate(parkInfoLinks);
+footerTemplate(parkData);
